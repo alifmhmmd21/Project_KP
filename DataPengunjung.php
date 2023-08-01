f2c9b51145dcaf3e4bc4bd88e2c76b76d770220a
Alif Muhammd Hanif <alif05mhmmd@gmail.com>
2023-08-01 15:49:25 +0700

    lagi

--- DataPengunjung.php
+++ DataPengunjung.php
@@ -43,7 +43,7 @@ $nomor = $halaman_awal+1;
         <button class="w3-bar-item w3-button w3-large"
         onclick="w3_close()">Close &times;</button>
         <a href="DataPenggunaVPS.php" class="w3-bar-item w3-button"> Data Pengguna VPS</a>
-        <a href="#" class="w3-bar-item w3-button">Link 2</a>
+        <a href="DataPengunjung.php" class="w3-bar-item w3-button">Data Pengunjung</a>
         <a href="#" class="w3-bar-item w3-button">Link 3</a>
         <a class= "w3-bar-item w3-button"  style="position: absolute; left: 0;bottom: 0;" href="logout.php">
             <i class="fa fa-power-off fa-lg"> 
