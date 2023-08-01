cced7ba78b249f560286dac73103d1aca9dcd1ce
mfaizgifari <muhammadfaizgifari28@gmail.com>
2023-08-01 16:13:01 +0700

    Edit sidebar

--- DataPenggunaHosting.php
+++ DataPenggunaHosting.php
@@ -42,8 +42,8 @@ $nomor = $halaman_awal + 1;
 
     <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
         <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
-        <a href="#" class="w3-bar-item w3-button">Data Pengguna Hosting</a>
-        <a href="#" class="w3-bar-item w3-button">Data Pengguna VPS</a>
+        <a href="DataPenggunaHosting.php" class="w3-bar-item w3-button">Data Pengguna Hosting</a>
+        <a href="DataPenggunaVPS.php" class="w3-bar-item w3-button">Data Pengguna VPS</a>
         <a href="#" class="w3-bar-item w3-button">Data Pengunjung</a>
         <a href="#" class="w3-bar-item w3-button">Data Pengajar</a>
         <a class="w3-bar-item w3-button" style="position: absolute; left: 0;bottom: 0;" href="logout.php">
