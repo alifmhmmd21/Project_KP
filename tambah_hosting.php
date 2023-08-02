2e9c8f9f8ac1bdae1e412a5355b549c23ae0a185
Kakai <Muhammadkaisan3@gmail.com>
2023-08-02 12:59:17 +0700

    edit by Kakai

---
+++ tambah_hosting.php
@@ -0,0 +1,18 @@
+<?php
+    require 'quer/config.php';
+            $no_ktp = $_POST['noktp'];
+            $nama=$_POST['nama'];
+            $alamat = $_POST['alamat'];
+            $no_hp = $_POST['nohp'];
+            $tujuan = $_POST['pesan'];
+
+            $akhir = "INSERT INTO visiter (no_ktp,nama,alamat,no_hp,tujuan) VALUES('$no_ktp','$nama','$alamat','$no_hp','$tujuan')";
+
+            $query = mysqli_query($conn,$akhir);
+            if($query){
+                header('location:DataPengunjung.php');
+                }else{
+                echo"Gagal Disimpan";
+            }
+
+?>
