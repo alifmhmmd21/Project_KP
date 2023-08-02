d79e29556da322d361c167f235def0b41a3fc1b9
Kakai <Muhammadkaisan3@gmail.com>
2023-08-03 00:47:26 +0700

    -

--- tambah_pengajar.php
+++ tambah_pengajar.php
@@ -5,7 +5,7 @@
             $name      = $_POST['name'];
             $email     = $_POST['email'];
             $phone     = $_POST['phone'];
-            $address    = $_POST['address'];
+            $address   = $_POST['address'];
             $materi    = $_POST['materi'];
 
             $akhir = "INSERT INTO pengajars (name,email,phone,address,materi) VALUES('$name','$email','$phone','$address','$materi')";
