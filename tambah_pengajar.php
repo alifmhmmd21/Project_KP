17556f727c334ec19a5fb30e275045915b8d2d08
Kakai <Muhammadkaisan3@gmail.com>
2023-08-02 09:34:42 +0700

    edit by kakai

---
+++ tambah_pengajar.php
@@ -0,0 +1,19 @@
+<!-- Insert Database -->
+<?php
+    require 'quer/config.php';
+            $nama      = $_POST['nama'];
+            $email     = $_POST['email'];
+            $phone     = $_POST['phone'];
+            $alamat    = $_POST['alamat'];
+            $materi    = $_POST['materi'];
+
+            $akhir = "INSERT INTO pengajar (nama,email,phone,alamat,materi) VALUES('$nama','$email','$phone','$alamat','$materi')";
+
+            $query = mysqli_query($conn,$akhir);
+            if($query){
+                header('location:DataPengajar.php');
+                }else{
+                echo"Gagal Disimpan";
+            }
+
+?>
