6296baec5d8f2a9e06fa2585982823f41e657db0
RavenFajar <ravenfajar2502@gmail.com>
2023-08-02 12:04:02 +0700

    edit by raven

--- ceklogin.php
+++ ceklogin.php
@@ -1,5 +1,9 @@
 <?php 
 session_start();
+if (isset($_SESSION['id']) != '') {
+    header("location:DataPengunjung.php");
+    exit();
+}
 include 'quer/config.php';
 
 $user		= stripcslashes($_POST['user']);
--- logout.php
+++ logout.php
@@ -1,8 +1,6 @@
 <?php 
- 
-session_start();
-session_destroy();
- 
+ session_start();
+ unset($_SESSION['id']);
+ session_destroy();
 header("Location: login.php");
- 
 ?>
