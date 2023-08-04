42666a557c304f8a4f3fb4cc7cda1c5022d0b9ea
RavenFajar <ravenfajar2502@gmail.com>
2023-08-04 11:00:28 +0700

    Edit By Raven

---
+++ dashboard/Home.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+    <section class="p-5">
+    <div class="container">
+        <div class="row text-center">
+        <div class="col-md">
+            <div class="card bg-primary text-light">
+                <div class="card-body text-center">
+                    <img src="../asset/X2.png" width="100 px">
+                    <h4>Data Pengguna Hosting</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellendus ex et nobis rem! Esse soluta, consequatur obcaecati culpa similique iste eius tenetur tempore sit dicta at aperiam id minima.
+
+                    </p>
+                    <button type="button" class="btn btn-primary">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="card bg-danger text-light">
+                <div class="card-body text-center">
+                    <img src="../asset/X2.png" width="100 px">
+                    <h4>Data Pengguna VPS</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellendus ex et nobis rem! Esse soluta, consequatur obcaecati culpa similique iste eius tenetur tempore sit dicta at aperiam id minima.
+
+                    </p>
+                    <button type="button" class="btn btn-primary">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="card bg-warning text-light">
+                <div class="card-body text-center">
+                    <img src="../asset/X2.png" width="100 px">
+                    <h4>Data Pengunjung</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellendus ex et nobis rem! Esse soluta, consequatur obcaecati culpa similique iste eius tenetur tempore sit dicta at aperiam id minima.
+
+                    </p>
+                    <button type="button" class="btn btn-primary">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="card bg-success text-light">
+                <div class="card-body text-center">
+                    <img src="../asset/X2.png" width="100 px">
+                    <h4>Data Pengajar</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellendus ex et nobis rem! Esse soluta, consequatur obcaecati culpa similique iste eius tenetur tempore sit dicta at aperiam id minima.
+
+                    </p>
+                    <button type="button" class="btn btn-primary">Selengkapnya</button>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </section>
+  </body>
+</html>
